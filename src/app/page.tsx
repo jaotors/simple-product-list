65e4b4da1d0a8045633d989afb5bcf91f7f4de89
Joshua Turingan <turingan.joshua@gmail.com>
2023-12-10 03:16:59 +0800

    Add more rules on ESLint
    - add eslint-config-prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 export default function Home() {
   return (
-    <div className='text-center'>
-      <h1 className='text-4xl'>Hello World</h1>
-      <h2 className='text-3xl'>Hello NextJS</h2>
+    <div className="text-center">
+      <h1 className="text-4xl">Hello World</h1>
+      <h2 className="text-3xl">Hello NextJS</h2>
 
-      <h3 className='text-2xl'>Learn Test</h3>
+      <h3 className="text-2xl">Learn Test</h3>
     </div>
   )
 }
