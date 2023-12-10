2720fa037dd8dc14e5cfc5694abda3fd04d54fd7
Joshua Turingan <turingan.joshua@gmail.com>
2023-12-11 06:16:57 +0800

    Change z-index of Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ import BasketPopover from '../BasketPopover'
 
 const Navbar = () => {
   return (
-    <div className="fixed top-0 w-full bg-white flex justify-between items-center border-b border-b-black px-1 py-2 z-10">
+    <div className="fixed top-0 w-full bg-white flex justify-between items-center border-b border-b-black px-1 py-2 z-20">
       <h1 className="text-3xl font-black text-gray-800">
         <a href="/">Product Logo</a>
       </h1>
