7916bf00b88ca62fece234bfffe4d0851ac1c8a2
Joshua Turingan <turingan.joshua@gmail.com>
2023-12-11 07:39:54 +0800

    Add more tests on ProductCard component

--- src/components/ProductCard/__tests__/PorductCard.test.tsx
+++ src/components/ProductCard/__tests__/PorductCard.test.tsx
@@ -1,1 +1,30 @@
 import { render, screen } from '@testing-library/react'
+import ProductCard from '../ProductCard'
+
+describe('Product Card component', () => {
+  const props = {
+    title: 'Product Card Title',
+    price: 20,
+    discountPercentage: 2.5,
+    rating: 4.5,
+    stock: 100,
+    brand: 'Brand',
+    image: '/imagepath',
+  }
+
+  it('should display the title', () => {
+    render(<ProductCard {...props} />)
+
+    const element = screen.getByText('Product Card Title')
+
+    expect(element).toBeInTheDocument()
+  })
+
+  it('should display the computed price', () => {
+    render(<ProductCard {...props} />)
+
+    const element = screen.getByText('PHP 19.50')
+
+    expect(element).toBeInTheDocument()
+  })
+})
