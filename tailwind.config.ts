c2417acb4e0eeffa0ca7da714591d8eead9c4b63
Joshua Turingan <turingan.joshua@gmail.com>
2023-12-11 04:09:26 +0800

    Add tailwind-merge lib
    - add custom shadow on tailwindcss

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      boxShadow: {
+        custom: '0 0 20px rgba(0,0,0, 0.1)',
+      },
     },
   },
   plugins: [],
